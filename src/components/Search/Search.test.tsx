ae06fe2adc790af84e7616b9a5474273db1bd6b4
Tran Le <axe3182003@gmail.com>
2025-09-20 12:54:52 +0700

    Add test for search

---
+++ src/components/Search/Search.test.tsx
@@ -0,0 +1,14 @@
+import { screen } from "@testing-library/react";
+import { expect, test } from "vitest";
+
+test("no keyword provided", () => {
+    expect(screen.getByRole("button"))
+})
+
+test("keyword with results provided", () => {
+
+})
+
+test("keyword with no results provided", () => {
+
+})
